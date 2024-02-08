933f81ecb138617630ed1070cfdb7dbacf9dae18
KOST <KSK>
2024-02-08 20:13:50 +0200

    add Task2: одно решение

---
+++ Lesson2/Task2/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задание 2. Обсуждение решений
+// Семинар 2. Простые алгоритмы на C#
+// Напишите программу, которая принимает на вход трёхзначное
+// число и возводит вторую цифру этого числа в степень, равную
+// третьей цифре.
+// Примеры
+// 487 => 8^7 = 2 097 152
+// 254 => 5^4 = 625
+// 617 => 1
+
+System.Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if ( number > 99 && number < 1000)
+{
+  int midldigit = number % 100 /10;
+  int rightDigit = number % 10;
+  int i = 0;
+  int result = 1;
+  while (i < ritdigit)
+  {
+    result = result * midldigit;
+    i ++;
+
+  }
+  System.Console.WriteLine(result);
+}
+else 
+{
+    System.Console.WriteLine("Ввели не трехзначное число");
+}

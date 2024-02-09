f8b7319f4a1ba51e8a172ba113a73512aade0183
KOST <KSK>
2024-02-09 19:44:21 +0200

    add Task5: Добавил решение вычесления всех четных чисел, через пробел до указанного числа

--- Lesson1/Task5/Program.cs
+++ Lesson1/Task5/Program.cs
@@ -1,10 +1,40 @@
-﻿int n = 100;
-int i = 1;
+﻿
+// Дано натуральное число N. Вывести на экран числа от 1 до N.
 
-while (i <= n)
+
+// int n = 100;
+// int i = 1;
+
+
+
+// while (i <= n) 
+//     {
+//     //  Console.Write(n + " ");
+//     //  Console.Write(i);
+//      Console.Write(i + " ");
+//       i = i + 1;
+//     }
+
+//     Задачка!!!!!!
+
+//  Вход принимает число (number),
+//  а на выходе выводит все чётные числа от 1 до number (включительно), 
+//  после каждого числа должен быть знак пробела.
+
+System.Console.Write("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+
+
+for (int i = 1; i <= N; i ++)
 {
-   // Console.WriteLine(i);
-    Console.Write(i);
-    Console.Write(' ');
-    i = i + 1;
+    if (i % 2 == 0)
+    {
+        Console.Write(i + " ");
+
+    }
+
 }
+
+
+
+

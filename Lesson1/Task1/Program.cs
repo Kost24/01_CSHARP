b4e62ff59b714457291255a2afe4fda25c27c9aa
KOST <KSK>
2024-02-09 19:40:29 +0200

    add Task1: Добавил решение на нахождение четного или нечетного числа

--- Lesson1/Task1/Program.cs
+++ Lesson1/Task1/Program.cs
@@ -1,12 +1,25 @@
 ﻿//Console.WriteLine("Hello World");
 
-int value = 100;
-Console.WriteLine(value);
+// int value = 100;
+// Console.WriteLine(value);
 
 
-double d = 5.55;
-char c = '!';
-bool  b = true;
-Console.WriteLine(d);
-Console.WriteLine(c);
-Console.WriteLine(b);
+// double d = 5.55;
+// char c = '!';
+// bool  b = true;
+// Console.WriteLine(d);
+// Console.WriteLine(c);
+// Console.WriteLine(b);
+
+int number = 9;
+
+if (number % 2 == 0) 
+      {
+        System.Console.WriteLine("Четное");
+      } 
+      else 
+      {
+          System.Console.WriteLine("Нечетное");
+      }
+
+
